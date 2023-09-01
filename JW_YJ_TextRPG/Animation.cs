9d0e857115b849d3e9f9877cc62b3e1b1d48762d
Jo Yongjun <yongjun5835@gmail.com>
2023-09-02 00:11:05 +0900

    애니메이션 추가

---
+++ JW_YJ_TextRPG/Animation.cs
@@ -0,0 +1,76 @@
+﻿using JW_YJ_TextRPG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Animation
+{
+    public Animation()
+    {
+
+    }
+
+    public void UnfoldScroll(int x, int y)
+    {
+        Console.Clear();
+        Program.ui.DrawScroll(x + 40, y, 0, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 35, y, 10, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 30, y, 20, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 25, y, 30, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 20, y, 40, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 15, y, 50, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 10, y, 60, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 5, y, 70, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x, y, 80, "");
+        Thread.Sleep(30);
+    }
+
+    public void FoldScroll(int x, int y)
+    {
+        Console.Clear();
+        Program.ui.DrawScroll(x, y, 80, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 5, y, 70, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 10, y, 60, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 15, y, 50, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 20, y, 40, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 25, y, 30, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 30, y, 20, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 35, y, 10, "");
+        Thread.Sleep(30);
+        Console.Clear();
+        Program.ui.DrawScroll(x + 40, y, 0, "");
+        Thread.Sleep(30);
+    }
+}
--- JW_YJ_TextRPG/Program.cs
+++ JW_YJ_TextRPG/Program.cs
@@ -2,10 +2,13 @@
 {
     internal class Program
     {
-        static UI ui = new UI();
+        public static UI ui = new UI();
+        public static Animation animation = new Animation();
 
         static void Main(string[] args)
         {
+            animation.UnfoldScroll(0, 0);
+            animation.FoldScroll(0, 0);
             ui.DrawUI(0, 0, 12, "놀라운 결과", "Gray");
             ui.DrawUI(0, 8, 12, "놀라운 결과", "");
             ui.DrawScroll(50, 0, 20, "");

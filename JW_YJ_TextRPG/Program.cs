beda26754e4f9f69227d400ad6e1b1199b325329
Jo Yongjun <yongjun5835@gmail.com>
2023-09-01 22:03:44 +0900

    DrawBox 메소드 추가

--- JW_YJ_TextRPG/Program.cs
+++ JW_YJ_TextRPG/Program.cs
@@ -7,6 +7,7 @@
         static void Main(string[] args)
         {
             ui.DrawUI(1, 0, 0, "테스트용!", "");
+            ui.DrawBox(10, 10, 50, 10, "");
             Console.SetCursorPosition(0, 20);
         }
     }
--- JW_YJ_TextRPG/UI.cs
+++ JW_YJ_TextRPG/UI.cs
@@ -45,4 +45,46 @@ internal class UI
             DrawText(x, y + 6, "               '-.-'", color);
         }
     }
+
+
+    // width, height 최솟값 3
+    public void DrawBox(int x, int y, int width, int height, string color)
+    {
+        if (color == "Green")
+            Console.ForegroundColor = ConsoleColor.Green;
+        else if (color == "Red")
+            Console.ForegroundColor = ConsoleColor.Red;
+        else if (color == "Blue")
+            Console.ForegroundColor = ConsoleColor.Blue;
+        else if (color == "Black")
+            Console.ForegroundColor = ConsoleColor.Black;
+        else if (color == "Gray")
+            Console.ForegroundColor = ConsoleColor.Gray;
+        else if (color == "Yellow")
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.SetCursorPosition(x, y);
+        Console.Write("┏");
+        for (int i = 1; i <= width - 2; i++)
+        {
+            Console.Write("━");
+        }
+        Console.SetCursorPosition(x + width - 1, y);
+        Console.Write("┓");
+        for (int i = 1; i <= height - 2; i++)
+        {
+            Console.SetCursorPosition(x, y + i);
+            Console.Write("┃");
+            Console.SetCursorPosition(x + width - 1, y + i);
+            Console.Write("┃");
+        }
+        Console.SetCursorPosition(x, y + height - 1);
+        Console.Write("┗");
+        for (int i = 1; i <= width - 2; i++)
+        {
+            Console.Write("━");
+        }
+        Console.SetCursorPosition(x + width - 1, y + height - 1);
+        Console.Write("┛");
+        Console.ResetColor();
+    }
 }

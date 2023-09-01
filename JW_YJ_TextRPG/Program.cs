2c6068560cc06260ea58d4db5136e0113051ca24
Jo Yongjun <yongjun5835@gmail.com>
2023-09-01 22:42:31 +0900

    DrawScroll 메소드 추가

--- JW_YJ_TextRPG/Program.cs
+++ JW_YJ_TextRPG/Program.cs
@@ -7,7 +7,7 @@
         static void Main(string[] args)
         {
             ui.DrawUI(1, 0, 0, "테스트용!", "");
-            ui.DrawBox(10, 10, 50, 10, "");
+            ui.DrawScroll(50, 0, 20, "");
             Console.SetCursorPosition(0, 20);
         }
     }
--- JW_YJ_TextRPG/UI.cs
+++ JW_YJ_TextRPG/UI.cs
@@ -87,4 +87,73 @@ internal class UI
         Console.Write("┛");
         Console.ResetColor();
     }
+
+    public void DrawScroll(int x, int y, int width, string color)
+    {
+        DrawText(x + 0, y + 0, "    ___", color);
+        DrawText(x + 0, y + 1, "   {   }", color);
+        DrawText(x + 0, y + 2, " ___)_(___", color);
+        DrawText(x + 0, y + 3, "|         |", color);
+        DrawText(x + 0, y + 4, "|        -|", color);
+        DrawText(x + 0, y + 5, "|      -  |", color);
+        DrawText(x + 0, y + 6, "|         |", color);
+        DrawText(x + 0, y + 7, "|        -|", color);
+        DrawText(x + 0, y + 8, "|         |", color);
+        DrawText(x + 0, y + 9, "|      -  |", color);
+        DrawText(x + 0, y + 10, "|         |", color);
+        DrawText(x + 0, y + 11, "|         |", color);
+        DrawText(x + 0, y + 12, "|      -  |", color);
+        DrawText(x + 0, y + 13, "|         |", color);
+        DrawText(x + 0, y + 14, "|        -|", color);
+        DrawText(x + 0, y + 15, "|         |", color);
+        DrawText(x + 0, y + 16, "|       - |", color);
+        DrawText(x + 0, y + 17, "|         |", color);
+        DrawText(x + 0, y + 18, "|        -|", color);
+        DrawText(x + 0, y + 19, "|         |", color);
+        DrawText(x + 0, y + 20, "|      -  |", color);
+        DrawText(x + 0, y + 21, "|         |", color);
+        DrawText(x + 0, y + 22, "|        -|", color);
+        DrawText(x + 0, y + 23, "|         |", color);
+        DrawText(x + 0, y + 24, "|     -   |", color);
+        DrawText(x + 0, y + 25, "|        -|", color);
+        DrawText(x + 0, y + 26, "|_________|", color);
+        DrawText(x + 0, y + 27, "    ) (", color);
+        DrawText(x + 0, y + 28, "   {___}", color);
+
+        DrawText(x + 11 + width, y + 0, "    ___", color);
+        DrawText(x + 11 + width, y + 1, "   {   }", color);
+        DrawText(x + 11 + width, y + 2, " ___)_(___", color);
+        DrawText(x + 11 + width, y + 3, "|         |", color);
+        DrawText(x + 11 + width, y + 4, "|-        |", color);
+        DrawText(x + 11 + width, y + 5, "|  -      |", color);
+        DrawText(x + 11 + width, y + 6, "|         |", color);
+        DrawText(x + 11 + width, y + 7, "|-        |", color);
+        DrawText(x + 11 + width, y + 8, "|         |", color);
+        DrawText(x + 11 + width, y + 9, "|   -     |", color);
+        DrawText(x + 11 + width, y + 10, "|         |", color);
+        DrawText(x + 11 + width, y + 11, "|-        |", color);
+        DrawText(x + 11 + width, y + 12, "|         |", color);
+        DrawText(x + 11 + width, y + 13, "|         |", color);
+        DrawText(x + 11 + width, y + 14, "|  -      |", color);
+        DrawText(x + 11 + width, y + 15, "|         |", color);
+        DrawText(x + 11 + width, y + 16, "|-        |", color);
+        DrawText(x + 11 + width, y + 17, "|         |", color);
+        DrawText(x + 11 + width, y + 18, "|    -    |", color);
+        DrawText(x + 11 + width, y + 19, "|         |", color);
+        DrawText(x + 11 + width, y + 20, "|-        |", color);
+        DrawText(x + 11 + width, y + 21, "|         |", color);
+        DrawText(x + 11 + width, y + 22, "|   -     |", color);
+        DrawText(x + 11 + width, y + 23, "|         |", color);
+        DrawText(x + 11 + width, y + 24, "|-        |", color);
+        DrawText(x + 11 + width, y + 25, "|         |", color);
+        DrawText(x + 11 + width, y + 26, "|_________|", color);
+        DrawText(x + 11 + width, y + 27, "    ) (", color);
+        DrawText(x + 11 + width, y + 28, "   {___}", color);
+
+        for (int i = 1; i <= width; i++)
+        {
+            DrawText(x + 10 + i, y + 3, "-", color);
+            DrawText(x + 10 + i, y + 26, "-", color);
+        }
+    }
 }

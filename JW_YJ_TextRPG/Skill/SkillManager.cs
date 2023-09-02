5af9a1305a41b4792632a2504cacaa616cbcfa8c
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2023-09-02 23:39:02 +0900

    Squashed commit of the following:
    commit 29f6ad159e65fd4da05058054cea19fe9872183c
    Merge: 6ffa08d 652c09d
    Author: Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
    Date:   Sat Sep 2 23:37:11 2023 +0900
    
        Merge branch 'main' into skill
    
    commit 6ffa08d43d784449ac63e78386014613ee974952
    Author: Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
    Date:   Sat Sep 2 23:33:18 2023 +0900
    
        add. class 파티션 나누기
    
        data부분이랑 manager부분 파티션만 나눴습니다 똑같이 manager로 접근하면됩니다!

--- JW_YJ_TextRPG/Skill/SkillManager.cs
+++ JW_YJ_TextRPG/Skill/SkillManager.cs
@@ -1,6 +1,6 @@
 ﻿using System.Runtime.InteropServices;
 
-class SkillManager
+partial class SkillManager
 {
     public static SkillManager SM;
 
@@ -69,107 +69,6 @@ class SkillManager
                 break;
         }
     } // skill 데이터 입력
-
-    void BodySlam(Skill skill)
-    {
-        skill.AttackType = AttackType.Nomal;
-        skill.Name = "몸통 박치기";
-        skill.Comment = "상대를 향하여 전속력 박치기를 한다.";
-        skill.UseComment = ", 가라 몸통 박치기!!";
-        skill.MaxPP = 20;
-        skill.PP = skill.MaxPP;
-        skill.Power = 0.8f;
-        skill.Accuracy = 0.9f;
-    }
-
-    void TailWhip(Skill skill)
-    {
-        skill.AttackType = AttackType.Def;
-        skill.Name = "꼬리 흔들기";
-        skill.Comment = "꼬리를 흔들어 상대를 도발한다.";
-        skill.UseComment = "은(는) 살랑살랑 꼬리를 흔들었다.";
-        skill.Power = 1.0f;
-        skill.MaxPP = 15;
-        skill.PP = skill.MaxPP;
-        skill.Accuracy = 1.0f;
-        ((BuffSkill)skill).EffectTurn = 3;
-        ((BuffSkill)skill).Percent = 0.2f;
-
-    }
-
-    void WaterCanon(Skill skill)
-    {
-        skill.AttackType = AttackType.Cut;
-        skill.Name = "물 대포";
-        skill.Comment = "물을 상대에게 발사한다.";
-        skill.UseComment = "은(는) 물 대포를 발사했다!!.";
-
-        skill.MaxPP = 10;
-        skill.PP = skill.MaxPP;
-        skill.Power = 1.4f;
-        skill.Accuracy = 0.9f;
-    }
-
-    void BubbleBeam(Skill skill)
-    {
-        skill.AttackType = AttackType.Nomal;
-        skill.Name = "거품 광선";
-        skill.Comment = "거품을 상대에게 발사한다.";
-        skill.UseComment = "은(는) 거품을 발사했다!!";
-        skill.MaxPP = 10;
-        skill.PP = skill.MaxPP;
-        skill.Power = 1.0f;
-        skill.Accuracy = 0.9f;
-    }
-
-    void IceBeam(Skill skill)
-    {
-        skill.AttackType = AttackType.Pierce;
-        skill.Name = "냉동 빔";
-        skill.Comment = "냉동빔을 상대에게 발사한다.";
-        skill.UseComment = "의 내애앵도오오옹비이임!!";
-        skill.MaxPP = 5;
-        skill.PP = skill.MaxPP;
-        skill.Power = 0.8f;
-        skill.Accuracy = 0.9f;
-    }
-
-    void Splash(Skill skill)
-    {
-        skill.AttackType = AttackType.Nomal;
-        skill.Name = "튀어오르기";
-        skill.Comment = "공격도 하지 않고 팔딱팔딱 튈 뿐";
-        skill.UseComment = "은(는) 팔딱 뛰었다. 아무 일도 없었다.";
-        skill.MaxPP = 50;
-        skill.PP = skill.MaxPP;
-        skill.Power = 0.0f;
-        skill.Accuracy = 0.0f;
-    }
-
-    void BiteDeep(Skill skill)
-    {
-        skill.AttackType = AttackType.Hp;
-        skill.Name = "깊게 물기";
-        skill.Comment = "상대에게 출혈을 유도한다.";
-        skill.UseComment = "은(는) 깊게 깨물었다.";
-        skill.MaxPP = 5;
-        skill.PP = skill.MaxPP;
-        skill.Power = 1.0f;
-        skill.Accuracy = 0.7f;
-        ((BuffSkill)skill).EffectTurn = 3;
-        ((BuffSkill)skill).Percent = 5;
-    }
-
-    void None(Skill skill)
-    {
-        skill.AttackType = AttackType.Hp;
-        skill.Name = "";
-        skill.Comment = "";
-        skill.MaxPP = 0;
-        skill.PP = skill.MaxPP;
-        skill.Power = 0f;
-        skill.Accuracy = 0f;
-    }
 
     public void CalcAttackType(Skill skill, Unit taget, ref int damage)
     {
---
+++ JW_YJ_TextRPG/Unit/UnitData.cs
@@ -0,0 +1,83 @@
+﻿partial class UnitManager
+{
+
+    void Player(Unit unit)
+    {
+        unit.Hp = 30;
+        unit.MaxHp = 30;
+        unit.Atk = 15;
+        unit.Def = 2;
+        unit.Spd = 5;
+        unit.Accuracy = 3;
+        unit.Dodge = 1;
+
+        SkillManager.SM.AddSkill(unit, SKillType.BodySlam);
+        SkillManager.SM.AddSkill(unit, SKillType.TailWhip);
+        unit.SkillList.Add(new AttackSkill(SKillType.None));
+        unit.SkillList.Add(new AttackSkill(SKillType.None));
+    }
+
+    void Scomber(Unit unit)
+    {
+        unit.Name = "고등어";
+        unit.Hp = 30;
+        unit.MaxHp = 30;
+        unit.Atk = 5;
+        unit.Def = 2;
+        unit.Spd = 5;
+        unit.Accuracy = 3;
+        unit.Dodge = 1;
+
+
+        SkillManager.SM.AddSkill(unit, SKillType.BodySlam);
+        SkillManager.SM.AddSkill(unit, SKillType.Splash);
+    }
+
+    void Shark(Unit unit)
+    {
+        unit.Name = "상어";
+        unit.Hp = 90;
+        unit.MaxHp = 90;
+        unit.Atk = 10;
+        unit.Def = 3;
+        unit.Spd = 10;
+        unit.Accuracy = 5;
+        unit.Dodge = 5;
+
+        SkillManager.SM.AddSkill(unit, SKillType.BodySlam);
+        SkillManager.SM.AddSkill(unit, SKillType.TailWhip);
+        SkillManager.SM.AddSkill(unit, SKillType.WaterCanon);
+    }
+
+    void Turtle(Unit unit)
+    {
+        unit.Name = "거북이";
+        unit.Hp = 150;
+        unit.MaxHp = 150;
+        unit.Atk = 5;
+        unit.Def = 10;
+        unit.Spd = 1;
+        unit.Accuracy = 1;
+        unit.Dodge = 1;
+
+        SkillManager.SM.AddSkill(unit, SKillType.BodySlam);
+        SkillManager.SM.AddSkill(unit, SKillType.TailWhip);
+        SkillManager.SM.AddSkill(unit, SKillType.BubbleBeam);
+    }
+
+    void Angler(Unit unit)
+    {
+        unit.Name = "낚시꾼";
+        unit.Hp = 210;
+        unit.MaxHp = 210;
+        unit.Atk = 20;
+        unit.Def = 5;
+        unit.Spd = 10;
+        unit.Accuracy = 5;
+        unit.Dodge = 5;
+
+        SkillManager.SM.AddSkill(unit, SKillType.BodySlam);
+        SkillManager.SM.AddSkill(unit, SKillType.TailWhip);
+        SkillManager.SM.AddSkill(unit, SKillType.WaterCanon);
+    }
+}

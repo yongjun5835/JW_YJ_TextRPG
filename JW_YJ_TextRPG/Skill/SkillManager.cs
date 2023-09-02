cf2c78345a82edd156aa6204fbef38b5bb6fe5fb
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2023-09-02 14:14:27 +0900

    add. 스킬 뼈대 추가

---
+++ JW_YJ_TextRPG/Skill/SkillManager.cs
@@ -0,0 +1,30 @@
+﻿
+class SkillManager
+{
+    public static SkillManager SM;
+    public SkillManager()
+    {
+        SM = this;
+    }
+
+    public void ChangeData(Skill skill)
+    {
+        switch (skill.SkillType)
+        {
+            case SKillType.BodySlam :
+                BodySlam(skill);
+                break;
+        }
+    }
+
+    void BodySlam(Skill skill)
+    {
+        skill.ElementType = ElementType.Nomal;
+        skill.Name = "몸통 박치기";
+        skill.Comment = "상대를 향하여 전속력 박치기를 한다.";
+        skill.Power = 1.0f;
+        skill.Accuracy = 0.9f;
+
+    }
+
+}

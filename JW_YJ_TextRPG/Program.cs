0211e23dc62b2f557156d668e720e656f905056e
Jo Yongjun <yongjun5835@gmail.com>
2023-09-02 13:50:27 +0900

    플레이어 생성씬 수정

--- JW_YJ_TextRPG/Program.cs
+++ JW_YJ_TextRPG/Program.cs
@@ -8,10 +8,7 @@
 
         static void Main(string[] args)
         {
-            animation.DrawTextSlowly(0, 0, "김치를 맛있게 먹는 방법은 고구마와 같이 먹는 것이다.", "Blue");
-            animation.FadeInOut(42, 10, 14, "반가워");
             createPlayer.DrawDisplay();
-            Console.SetCursorPosition(0, 20);
         }
     }
 }
